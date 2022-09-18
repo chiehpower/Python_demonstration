471f84587432a7af90f6db032ec96746f06e3c05
Chieh <iloveberit@gmail.com>
2022-09-18 14:22:06 +0800

    update menu

--- streamlit/streamlit-elements-testing/Apply_MUI.py
+++ streamlit/streamlit-elements-testing/Apply_MUI.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_elements import elements, sync, event, mui
+from streamlit_elements import elements, mui
 
 if __name__ == '__main__':
 
@@ -11,8 +11,8 @@ if __name__ == '__main__':
         st.session_state.item = ""
     
     class handle:
-        def handle_menu1(self, event):
-            st.session_state.item = '1'
+        def handle_menu1(self):
+            st.session_state.item = 'Item1'
 
         def handle_open2(event):
             if st.session_state.open2 == False:
@@ -20,16 +20,16 @@ if __name__ == '__main__':
             else:
                 st.session_state.open2 = False
         
-        def handle_menu2(self, event):
-            st.session_state.item = '2-1'
-        def handle_menu3(self, event):
-            st.session_state.item = '2-2'
+        def handle_menu2(self):
+            st.session_state.item = 'Item2-1'
+        def handle_menu3(self):
+            st.session_state.item = 'Item2-2'
 
-        def handle_menu4(self, event):
-            st.session_state.item = '3-1'
-        def handle_menu5(self, event):
-            st.session_state.item = '3-2'
-        def handle_open3(event):
+        def handle_menu4(self):
+            st.session_state.item = 'Item3-1'
+        def handle_menu5(self):
+            st.session_state.item = 'Item3-2'
+        def handle_open3(self):
             if st.session_state.open3 == False:
                 st.session_state.open3 = True
             else:
@@ -41,18 +41,18 @@ if __name__ == '__main__':
         handle_menu = handle()
         with elements("style_elements_cssss"):
             with st.sidebar:
-                with mui.List(component="nav", sx={".Mui-selected":{"backgroundColor":"#e44444 !important"}}):
+                with mui.List(component="nav", sx={".Mui-selected": {"backgroundColor": "#EBD6D6 !important"}}):
                     ### First
-                    with mui.ListItemButton(selected=True if st.session_state.item == 'Item1' else False ):
+                    with mui.ListItemButton(selected=True if st.session_state.item == 'Item1' else False):
                         with mui.ListItemIcon:
                             # mui.icon.Send(sx={"color": "#ba68c8"})
-                            mui.icon.Add() # sx={"color": "#eb8c07"
+                            mui.icon.Add(sx={"color": "#eb8c07"}) # sx={"color": "#eb8c07"
                         mui.ListItemText('Item1', onClick=handle_menu.handle_menu1)
 
                     ### Second
                     with mui.ListItemButton(onClick=handle_menu.handle_open2):
                         with mui.ListItemIcon:
-                            mui.icon.Send()
+                            mui.icon.Send(sx={"color": "#eb8c07"})
                         mui.ListItemText('Item2')
                         if st.session_state.open2:
                             mui.icon.ExpandLess()
@@ -60,19 +60,19 @@ if __name__ == '__main__':
                             mui.icon.ExpandMore()
                     with mui.Collapse(in_=st.session_state.open2):
                         with mui.List(component="div", disablePadding=True):
-                            with mui.ListItemButton(onClick=handle_menu.handle_menu2, sx={"pl": 4}):
+                            with mui.ListItemButton(onClick=handle_menu.handle_menu2, sx={"pl": 4}, selected=True if st.session_state.item == 'Item2-1' else False):
                                 with mui.ListItemIcon:
-                                    mui.icon.Send()
+                                    mui.icon.Send(sx={"color": "#eb8c07"})
                                 mui.ListItemText('Item2-1')
-                            with mui.ListItemButton(onClick=handle_menu.handle_menu3, sx={"pl": 4}):
+                            with mui.ListItemButton(onClick=handle_menu.handle_menu3, sx={"pl": 4}, selected=True if st.session_state.item == 'Item2-2' else False):
                                 with mui.ListItemIcon:
-                                    mui.icon.Send()
+                                    mui.icon.Send(sx={"color": "#eb8c07"})
                                 mui.ListItemText('Item2-2')
 
                     ### Third
                     with mui.ListItemButton(onClick=handle_menu.handle_open3):
                         with mui.ListItemIcon:
-                            mui.icon.Send()
+                            mui.icon.Send(sx={"color": "#eb8c07"})
                         mui.ListItemText('Item3')
                         if st.session_state.open3:
                             mui.icon.ExpandLess()
@@ -80,15 +80,17 @@ if __name__ == '__main__':
                             mui.icon.ExpandMore()
                     with mui.Collapse(in_=st.session_state.open3):
                         with mui.List(component="div", disablePadding=True):
-                            with mui.ListItemButton(onClick=handle_menu.handle_menu4, sx={"pl": 4}):
+                            with mui.ListItemButton(onClick=handle_menu.handle_menu4, sx={"pl": 4}, selected=True if st.session_state.item == 'Item3-1' else False):
                                 with mui.ListItemIcon:
-                                    mui.icon.Send()
+                                    mui.icon.Send(sx={"color": "#eb8c07"})
                                 mui.ListItemText('Item3-1')
-                            with mui.ListItemButton(onClick=handle_menu.handle_menu5, sx={"pl": 4}):
+                            with mui.ListItemButton(onClick=handle_menu.handle_menu5, sx={"pl": 4}, selected=True if st.session_state.item == 'Item3-2' else False):
                                 with mui.ListItemIcon:
-                                    mui.icon.Send()
+                                    mui.icon.Send(sx={"color": "#eb8c07"})
                                 mui.ListItemText('Item3-2')
 
-    if st.session_state.item == '1':
+    st.write(st.session_state.item)
+    if st.session_state.item == 'Item1':
+        st.write("---")
         tool_name = st.selectbox("Choose the tool.", ["Test"])
         st.write(tool_name)
