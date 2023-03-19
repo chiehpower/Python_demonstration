abb27f10055002b9bcab0623f459f940175b895c
chiehpower <iloveberit@gmail.com>
2023-03-19 23:33:02 +0800

    update

--- xyPython/test.py
+++ xyPython/test.py
@@ -1,14 +1,46 @@
-# First things, first. Import the wxPython package.
 import wx
+import cv2
+import requests
 
-# Next, create an application object.
+class ShowCapture(wx.Panel):
+    def __init__(self, parent, capture, fps=15):
+        wx.Panel.__init__(self, parent)
+
+        self.capture = capture
+        ret, frame = self.capture.read()
+        print(frame.shape)
+        height, width = frame.shape[:2]
+        parent.SetSize((width, height))
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        self.bmp = wx.BitmapFromBuffer(width, height, frame)
+
+        self.timer = wx.Timer(self)
+        self.timer.Start(1000./fps)
+
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Bind(wx.EVT_TIMER, self.NextFrame)
+
+
+    def OnPaint(self, evt):
+        dc = wx.BufferedPaintDC(self)
+        dc.DrawBitmap(self.bmp, 0, 0)
+
+    def NextFrame(self, event):
+        ret, frame = self.capture.read()
+        if ret:
+            
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            self.bmp.CopyFromBuffer(frame)
+            self.Refresh()
+
+
+capture = cv2.VideoCapture(0)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
 app = wx.App()
-
-# Then a frame.
-frm = wx.Frame(None, title="Hello World")
-
-# Show it.
-frm.Show()
-
-# Start the event loop.
+frame = wx.Frame(None)
+cap = ShowCapture(frame, capture)
+frame.Show()
 app.MainLoop()
