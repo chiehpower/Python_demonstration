fea12616233653cd848367c6234156f37b80a713
Chieh <chieh_tsai@solomon-3d.com>
2024-02-04 18:51:52 +0800

    update

--- Line/fastapi-server/server/main.py
+++ Line/fastapi-server/server/main.py
@@ -1,21 +1,25 @@
 from fastapi import FastAPI, HTTPException, Form
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
 
 app = FastAPI()
 
 @app.post("/send-line-notification/")
 async def send_line_notification(message: str = Form(...)):
 
-    # key_ = ''
-    # headers = {
-    #     "Authorization": "Bearer " + key_, 
-    #     "Content-Type" : "application/x-www-form-urlencoded"
-    # }
+    headers = {
+        "Authorization": "Bearer " + TOKEN, 
+    }
 
-    # payload = {
-    #     'message': 'Hi guys' 
-    # }
-
+    files = {'imageFile': open("client_file.png", 'rb')}
+    payload = {
+        'message': 'Hi guys' 
+    }
+    r = requests.post("https://notify-api.line.me/api/notify", headers=headers, params=payload, files = files)
     # r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
 
     print(f"Received Line notification: {message}")
