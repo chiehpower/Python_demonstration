df7208707a7bc4d52a190a0427f7535eee15a8e4
Chieh <chieh_tsai@solomon-3d.com>
2024-05-21 09:44:00 +0800

    update

--- flask_demo/client.py
+++ flask_demo/client.py
@@ -3,11 +3,11 @@ import time
 
 start = time.time()
 
-files  = {'files': open('test.png', 'rb')}
+files = {'files': open('test.png', 'rb')}
 
-ip = '' # your server ip
+ip = ''  # your server ip
 url = 'http://{}:5001/normal_sending'.format(ip)
 
-result  = requests.post(url, files=files)
+result = requests.post(url, files=files)
 print(result.json())
-print("Total time: ", time.time()-start)
+print("Total time: ", time.time() - start)
--- flask_demo/server.py
+++ flask_demo/server.py
@@ -5,29 +5,30 @@ import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/devel',  methods=['POST', 'GET'])
+
+@app.route('/devel', methods=['POST', 'GET'])
 def devel():
     return jsonify({
-        'result':'Get to the devel'
+        'result': 'Get to the devel'
     })
 
-@app.route('/normal_sending' ,methods=['POST'])
-def predict():  
+
+@app.route('/normal_sending', methods=['POST'])
+def predict():
     start_pre = time.time()
     if request.method == 'POST':
         if 'files' not in request.files:
-           return jsonify({
-            'result':'someting went wrong 1'
-        })
+            return jsonify({'result': 'someting went wrong 1'})
 
         npimg = np.fromstring(request.files["files"].read(), np.uint8)
         img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
 
-        print('request files checking takes: ', time.time()-start_pre, flush=True)
+        print('request files checking takes: ', time.time() - start_pre, flush=True)
         return jsonify({
-            'result':'Get to the devel'
+            'result': 'Get to the devel'
         })
+
 
 if __name__ == '__main__':
     # This is used when running locally.
-    app.run(host='0.0.0.0',port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
